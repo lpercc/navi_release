4f1015b8a3f0e7edc2e6910a092643a1cf027558
liminghan <liminghan@galbot.com>
2025-11-11 13:50:16 +0800

    lmh/20251111/CHANGE HOME

--- unitree_ros2/cyclonedds_ws/install/unitree_go/share/unitree_go/local_setup.sh
+++ unitree_ros2/cyclonedds_ws/install/unitree_go/share/unitree_go/local_setup.sh
@@ -2,7 +2,7 @@
 
 # since this file is sourced use either the provided AMENT_CURRENT_PREFIX
 # or fall back to the destination set at configure time
-: ${AMENT_CURRENT_PREFIX:="/home/galbot/unitree_ros2/cyclonedds_ws/install/unitree_go"}
+: ${AMENT_CURRENT_PREFIX:="$HOME/unitree_ros2/cyclonedds_ws/install/unitree_go"}
 if [ ! -d "$AMENT_CURRENT_PREFIX" ]; then
   if [ -z "$COLCON_CURRENT_PREFIX" ]; then
     echo "The compile time prefix path '$AMENT_CURRENT_PREFIX' doesn't " \
